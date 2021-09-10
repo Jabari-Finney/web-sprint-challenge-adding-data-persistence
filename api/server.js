708bd3ca1b522d577a2e702293fa1083a451ad7f
Jabari-Finney <jabaritf@gmail.com>
2021-09-10 19:43:38 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
 // build your server here and require it from index.js
+const express = require('express')
+const server = express()
+const resourceRouter = require('./resource/router')
+const projectsRouter = require('./project/router')
+const tasksRouter = require('./task/router')
+
+
+server.use(express.json())
+
+server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectsRouter)
+server.use('/api/tasks', tasksRouter)
+
+server.use('*', (req, res, next) => {
+    next({status: 404, message: 'not found'})
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server
